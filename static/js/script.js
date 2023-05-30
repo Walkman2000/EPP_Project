1b60fc24a88ded8f28b8e9feec44148ee0afce05
Walkman2000 <yisusyelvino@gmail.com>
2023-05-30 14:13:11 -0600

    cambibos

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,38 @@
 /* ver carrito */
 
-const btnCart = document.querySelector('.container-icon')
+const btnCart = document.querySelector('.container-cart-icon')
     const containerCartProducts = document.querySelector('.container-cart-products')
 
     btnCart.addEventListener('click', () => {
       containerCartProducts.classList.toggle('hidden-cart')
     })
 
+/* agregar productos a carrito */
+const cartInfo = document.querySelector('.cart-product')
+const rowProduct = document.querySelector('.row-product')
 
-/* ver mas */
+const productList = document.querySelector('.content')
+
+let allProducts = []
+
+
+productList.addEventListener('click', e => {
+    if(e.target.classList.contains('btn-card comprar')){
+        console.log(e.target.parentElement)
+        
+
+
+
+    }
+
+})
+/*  fin  */
+
+/* ver mas 
 document.querySelectorAll(".ver").forEach(element => {
     const contenido = element.parentElement.parentElement.querySelector(".content");
     element.addEventListener("click", function () {
         contenido.classList.toggle("active")
         contenido.style.overflowY = "scroll";
     })
-})
+})*/
